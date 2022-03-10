3dda78d94773079dd76e061e3ea46ab6b336923a
SYSTEMS\junaid.yaqoob <junaidyaqoob@systemsltd.com>
2022-03-10 18:31:17 +0500

    top bar

---
+++ javascript/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.slider-1').slick({
+        infinite: true,
+        dots:false,
+        arrows:false,
+        autoplay:true,
+        autoplaySpeed:4000,
+        slidesToShow: 1,
+        slidesToScroll:1
+      });
+});
